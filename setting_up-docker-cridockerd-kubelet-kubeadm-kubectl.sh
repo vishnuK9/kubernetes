45bf23563ab1b844a10b64a536252e3ce3655119
vishnuK9 <48291740+vishnuK9@users.noreply.github.com>
2023-05-04 17:46:09 +0530

    Update setting_up-docker-cridockerd-kubelet-kubeadm-kubectl.sh

--- setting_up-docker-cridockerd-kubelet-kubeadm-kubectl.sh
+++ setting_up-docker-cridockerd-kubelet-kubeadm-kubectl.sh
@@ -64,6 +64,10 @@ sudo apt-mark hold kubelet kubeadm kubectl
 
 kubeadm init --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/cri-dockerd.sock
 
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
 
